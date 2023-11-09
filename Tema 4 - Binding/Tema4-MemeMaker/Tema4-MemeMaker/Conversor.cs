835777ed1bff71c2f53d2620cc90e6d269ef1440
JESÚS SVETOSLAV KRASTEV ZOTEV <jeskrazot@alu.edu.gva.es>
2023-11-09 09:05:45 +0100

    Terminado ejercicio MemeMaker

---
+++ Tema 4 - Binding/Tema4-MemeMaker/Tema4-MemeMaker/Conversor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Tema4_MemeMaker {
+    class ConversorCheckBoxBorder : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => ((bool) value) ? 2 : 0;    
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+}
