d213a92a743502e351c5d5342da0fc5d54fa9a7e
JESÚS SVETOSLAV KRASTEV ZOTEV <jeskrazot@alu.edu.gva.es>
2023-12-01 08:11:37 +0100

    Terminado Ejercicio Plantillas

---
+++ Tema 5 - Plantillas/Comida/Comida/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Comida
+{
+    public partial class MainWindow : Window
+    {
+        private MainWindowVM vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            vm = new MainWindowVM();
+            this.DataContext = vm;
+            vm.DeshabilitarEditorPlato(editorPlato);
+        }
+
+        private void Plato_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e) => vm.HabilitarEditorPlato(editorPlato);
+
+        private void BtnLimpiarSeleccion_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) => vm.DeshabilitarEditorPlato(editorPlato);
+    }
+}
---
+++ Tema 5 - Plantillas/Comida/Comida/MainWindowVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Comida
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        private ObservableCollection<Plato> listaPlatos;
+        public ObservableCollection<Plato> ListaPlatos
+        {
+            get { return listaPlatos; }
+            set
+            {
+                listaPlatos = value;
+                NotifyPropertyChanged("ListaPlatos");
+            }
+        }
+
+        private Plato platoSeleccionado;
+        public Plato PlatoSeleccionado
+        {
+            get { return platoSeleccionado; }
+            set
+            {
+                platoSeleccionado = value;
+                NotifyPropertyChanged("PlatoSeleccionado");
+            }
+        }
+
+
+        public MainWindowVM() 
+        {
+            ListaPlatos = Plato.GetSamples("./Resources/");
+            PlatoSeleccionado = null;
+        }
+
+        public void HabilitarEditorPlato(StackPanel editorPlato) => editorPlato.IsEnabled = true;
+        public void DeshabilitarEditorPlato(StackPanel editorPlato)
+        {
+            PlatoSeleccionado = null;
+            editorPlato.IsEnabled = false;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
